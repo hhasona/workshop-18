10cd74ce5618f500e0e5be5b9750abb31eead78e
hhasona <hhasona@gmail.com>
2022-07-15 11:46:34 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-const logger = require('pino')()
-const { Cube } = require('./cube');
+const logger = require("pino")()
+const { Cube } = require("./cube")
 
-var express = require('express');
-var app = express();
+var express = require("express")
+var app = express()
 
-app.get('/', function (req, res) {
-  const LENGTH = 5;
+app.get("/", function (req, res) {
+  const LENGTH = 5
   logger.info(`Createing a cube with length: ${LENGTH}`)
-  const cube = new Cube(5);
-  const properties = `Cube properties: length=${cube.getSideLength()}, surface=${cube.getSurfaceArea()}, volume=${cube.getVolume()}`;
-  logger.info(properties);
+  const cube = new Cube(5)
+  const properties = `Cube properties: length=${cube.getSideLength()}, surface=${cube.getSurfaceArea()}, volume=${cube.getVolume()}`
+  logger.info(properties)
 
-  res.send(properties);
+  res.send(properties)
 })
 
 var server = app.listen(process.env.PORT || 8081, function () {
